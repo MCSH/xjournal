922e88b4c350d5c1642d9666872fe20b33d4c223
MCSH <mcshemail@gmail.com>
2016-07-01 15:59:19 +0430

    Added commands, Command Mode

--- main.py
+++ main.py
@@ -25,53 +25,71 @@ class CWindow():
         self.text += text
         self.set_text(self.text)
 
-    def get_command(self, scr):
-        bak_text = self.text
-        self.set_text(":")
+
+class CommandMode():
+    def handle(self, app):
+        cwindow = app.cwin
+        bak_text = cwindow.text
         text = ""
         while True:
-            c = scr.getch()
+            c = app.screen.getch()
             if c == 27:
-                n = scr.getch()
+                n = app.screen.getch()
                 if n == curses.ERR:
-                    self.set_text(bak_text)
-                    return None
-                else:
-                    pass
+                    #ESC
+                    cwindow.set_text(bak_text)
+                    app.set_mode('normal')
+                    return
             elif c == curses.KEY_ENTER or c == 10 or c == 13:
-                return text
-            elif c in range(0, 128):
+                app.set_mode('normal')
+                app.command(text)
+                return
+            elif c in range(0,128):
                 text += chr(c)
-                self.add_text(chr(c))
+                cwindow.add_text(chr(c))
 
 class NormalMode():
     def handle(self, app):
         while True:
             c = app.screen.getch()
             if c == ord('q'):
+                app.command('quit')
                 return
             elif c == ord(':'):
-                app.command.get_command(self.screen)
+                app.set_mode('command')
+                return
             elif c == 27:
                 n = app.screen.getch()
                 if n == curses.ERR:
                     #ESC
-                    app.command.set_text('esc')
+                    app.cwin.set_text('esc')
                 else:
                     #ALT + n
-                    app.command.set_text(chr(n))
+                    app.cwin.set_text(chr(n))
             elif c == ord('p'):
-                app.command.add_text('woo')
-
+                app.command('write', text = 'Yolo')
 
 
 class App():
     def __init__(self):
+        self.run = True
+        self.modes = {}
+        self.commands = {}
         pass
 
     def initialize(self):
         os.environ.setdefault('ESCDELAY', '25')
-        self.set_mode(NormalMode())
+        self.add_mode(NormalMode(), 'normal')
+        self.add_mode(CommandMode(), 'command')
+        self.set_mode('normal')
+        def f(app, **kwargs):
+            app.run = False
+        self.add_command('quit',f)
+        def f(app, **kwargs):
+            if 'text' not in kwargs:
+                return
+            app.cwin.set_text( kwargs.get('text'))
+        self.add_command('write', f)
 
     def startscr(self, screen):
         self.screen = screen
@@ -79,14 +97,24 @@ class App():
         self.screen.clear()
         self.screen.immedok(True)
         self.screen.border()
-        self.command = CWindow()
+        self.cwin = CWindow()
         self.loop()
 
+    def add_command(self, command, func):
+        self.commands.update({command: func})
+
+    def command(self, command, **kwargs):
+        self.commands.get(command)(self,**kwargs)
+
+    def add_mode(self, mode, tag):
+        self.modes.update({tag: mode})
+
     def set_mode(self, mode):
-        self.mode = mode
+        self.mode = self.modes.get(mode)
 
     def loop(self):
-        self.mode.handle(self)
+        while self.run:
+            self.mode.handle(self)
 
 if __name__ == "__main__":
     app = App()
